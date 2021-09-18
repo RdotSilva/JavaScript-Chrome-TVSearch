4cd5434eea185d9457d9d3da07df4014f3670bd9
RdotSilva <RyandotSilva@gmail.com>
2021-09-17 20:35:05 -0400

    Remove unused context menus

--- background.js
+++ background.js
@@ -7,18 +7,6 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.onClicked.addListener((event) => {
     console.log(event);
   });
-  chrome.contextMenus.create({
-    title: "Test Context Menu 2",
-    id: "contextMenu2",
-    parentId: "contextMenu",
-    contexts: ["page", "selection"], // Only show context when user clicks page or selects
-  });
-  chrome.contextMenus.create({
-    title: "Test Context Menu 3",
-    id: "contextMenu3",
-    parentId: "contextMenu2",
-    contexts: ["page", "selection"], // Only show context when user clicks page or selects
-  });
 });
 
 console.log("Background script running");
