aea4062399d43adb369f1ca7f1fc7694fa9d2fa6
RdotSilva <RyandotSilva@gmail.com>
2021-09-21 17:42:09 -0400

    Add renderShow function

--- popup/popup.js
+++ popup/popup.js
@@ -2,3 +2,17 @@ chrome.storage.local.get(["shows"], (res) => {
   console.log(res);
   // TODO: Display tv show data instead of logging
 });
+
+function renderShow(show) {
+  const showDiv = document.createElement("div");
+
+  const title = document.createElement("h3");
+  title.textContent = show.show.name;
+
+  const image = document.createElement("img");
+  image.src = show.show.image.medium;
+
+  showDiv.appendChild(title);
+  showDiv.appendChild(image);
+  document.body.appendChild(showDiv);
+}
